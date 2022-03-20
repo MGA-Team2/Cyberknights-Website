6e28c2e1eecfe5ce482c4e8ecb5bbf007faf679b
Timothyx03 <99455134+Timothyx03@users.noreply.github.com>
2022-03-20 04:34:43 -0400

    DarkMode Good?

--- .github/MGA_Cyberknights/Darkmode.js
+++ .github/MGA_Cyberknights/Darkmode.js
@@ -1,25 +1,16 @@
-<script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js"></script>
-<script>
-function addDarkmodeWidget() {
-new Darkmode().showWidget();
-}
-window.addEventListener('load', addDarkmodeWidget);
-</script>
-<script>
 const options = {
-bottom: '64px', // default: '32px'
-right: 'unset', // default: '32px'
-left: '32px', // default: 'unset'
-time: '0.3s', // default: '0.3s'
-mixColor: '#fff', // default: '#fff'
-backgroundColor: '#fff',  // default: '#fff'
-buttonColorDark: '#100f2c',  // default: '#100f2c'
-buttonColorLight: '#fff', // default: '#fff'
-saveInCookies: true, // default: true,
-label: '🌓', // default: ''
-autoMatchOsTheme: true // default: true
+    bottom: '64px', // default: '32px'
+    right: '32px', // default: '32px'
+    left: 'unset', // default: 'unset'
+    time: '0.3s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: true, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true // default: true
 }
 
 const darkmode = new Darkmode(options);
 darkmode.showWidget();
-</script>

e7e6d80e12ec0581004a0816fd5745abe41ff8cf
Timothyx03 <99455134+Timothyx03@users.noreply.github.com>
2022-04-17 18:43:13 -0400

    NEW NAVBAR for Accessibility

--- .github/MGA_Cyberknights/Darkmode.js
+++ .github/MGA_Cyberknights/Darkmode.js
@@ -14,3 +14,10 @@ const options = {
 
 const darkmode = new Darkmode(options);
 darkmode.showWidget();
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
